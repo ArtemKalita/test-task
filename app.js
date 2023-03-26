0cbdd9f46c83b42f2a848320183d6cc468465c8b
artem kalita <a1@192.168.100.11>
2023-03-26 17:27:40 +0300

    version_2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const card = document.querySelector(".modal");
 function test(id) {
   const ajaxHandlerScript =
     "https://rickandmortyapi.com/api/character?page=" + id;
@@ -19,46 +20,15 @@ function test(id) {
         a.appendChild(name);
 
         dive.addEventListener("click", (ev) => {
-          const f = `<div id="openModal" class="modal">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h3 class="modal-title"> ${element.name}</h3>
-                  <button onclick="del()" class="close">x</button>
-                </div>
-                <div class="modal-body">
-                    <img src=${JSON.stringify(img)} class="img_1" alt="${
-            element.name
-          }">
-                    <div class="info">
-                        <div class="left">
-                            <div class="card_info modal-title">Name:<br> <span>${
-                              element.name
-                            }</span></div>
-                            <div class="card_info modal-title">Status: <br> <span>${
-                              element.status
-                            }</span></div>
-                            <div class="card_info modal-title">Species: <br> <span>${
-                              element.species
-                            }</span></div>
-                        </div>
-                        <div class="right">
-                            <div class="card_info modal-title">Origin:<br> <span>${
-                              element.origin.name
-                            }</span></div>
-                            <div class="card_info modal-title">Location:<br> <span>${
-                              element.location.name
-                            }</span></div>
-                            <div class="card_info modal-title">Gender:<br> <span>${
-                              element.gender
-                            }</span></div>
-                        </div>
-                    </div>
-                </div>
-              </div>
-            </div>
-          </div>`;
-          document.querySelector(".ccc").innerHTML = f;
+          card.style.display = "block";
+          document.querySelector(".modal-title").textContent = element.name;
+          document.querySelector(".img_1").src = element.image;
+          document.querySelector(".span_1").textContent = element.name;
+          document.querySelector(".span_2").textContent = element.status;
+          document.querySelector(".span_3").textContent = element.species;
+          document.querySelector(".span_4").textContent = element.origin.name;
+          document.querySelector(".span_5").textContent = element.location.name;
+          document.querySelector(".span_6").textContent = element.gender;
         });
         document.body.appendChild(dive);
       });
@@ -66,12 +36,10 @@ function test(id) {
   });
 }
 const del = () => {
-  document.querySelector(".modal").remove();
+  card.style.display = "none";
 };
-
 let e = 1;
 test(e);
-
 window.onscroll = function (ev) {
   if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
     e++;
